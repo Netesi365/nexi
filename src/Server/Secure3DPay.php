5e7649db988b47951d9c4e5d2f7d06ff4ebf182c
unknown <cto@netesi365.com>
2023-10-02 10:39:14 +0200

    Bug Fix Server\Secure3D

--- src/Server/Secure3DPay.php
+++ src/Server/Secure3DPay.php
@@ -8,7 +8,7 @@ use GuzzleHttp\RequestOptions;
 use GuzzleHttp\Exception\BadResponseException;
 use Composer\CaBundle\CaBundle;
 
-class Secure3D extends Nexirequest
+class Secure3DPay extends Nexirequest
 {
 	public function action(string $codTrans = '', string $importo = '', string $divisa = '', string $pan = '', string $scadenza = '', string $cvv = '', string $returnUrl = '') : string {
 		try {
