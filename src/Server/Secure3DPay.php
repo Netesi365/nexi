ce59c7f0b77572178eb6324789b272fb1eab2695
unknown <cto@netesi365.com>
2023-10-02 12:29:33 +0200

    Bug Fix Server\Secure3DPay

--- src/Server/Secure3DPay.php
+++ src/Server/Secure3DPay.php
@@ -10,14 +10,13 @@ use Composer\CaBundle\CaBundle;
 
 class Secure3DPay extends Nexirequest
 {
-	public function action(string $codTrans = '', string $importo = '', string $divisa = '', string $params) : string {
+	public function check(string $params = '') : string {
 		try {
 			if (!is_array(json_decode($params, true))) {
 				throw new \Exception('Params is not a JSON',001);
 			}
-			$request = json_decode($params, true);
-			
-			if (empty($codTrans) || empty($importo) || empty($divisa) || empty($request['esito']) || empty($request['messaggio']) || empty($request['idOperazione']) || empty($request['xpayNonce']) || empty($request['timeStamp']) || empty($request['mac'])) {
+			$request = json_decode($params, true);		
+			if (empty($request['esito']) || empty($request['messaggio']) || empty($request['idOperazione']) || empty($request['xpayNonce']) || empty($request['timeStamp']) || empty($request['mac'])) {
 				throw new \Exception('Missing a parameter',002);
 			}
 			if($request['esito'] != "OK"){
@@ -27,15 +26,42 @@ class Secure3DPay extends Nexirequest
 			if ($macCalculated != $request['mac']) {
 				throw new \Exception('Errore MAC:' . $macCalculated .' differs from'. $request['mac'],004);
 			}
+			$result = [
+					'success' => 1,
+					'error' => 0,
+					'code' => '0',
+					'idOperazione' => $request['idOperazione'],
+					'timeStamp' => $request['timeStamp'],
+					'msg' => 'OK'
+			];
+		}
+		catch (\Exception $e) {
+			$result = [
+					'success' => 0,
+					'error' => 1,
+					'code' => $e->getCode(),
+					'idOperazione' => $request['idOperazione'],
+					'timeStamp' => $request['timeStamp'],
+					'msg' => $e->getMessage()
+			];
+		}
+		$response = json_encode($result);
+		return $response;
+	}
+	public function action(string $codTrans = '', string $importo = '', string $divisa = '', string $xpayNonce = '') : string {
+		try {
+			if (empty($codTrans) || empty($importo) || empty($divisa) || empty($xpayNonce)) {
+				throw new \Exception('Missing a parameter',002);
+			}
 			$timeStamp = (time()) * 1000;
-			$mac = sha1('apiKey=' . $this->apiKey . 'codiceTransazione=' . $codTrans  . 'importo=' . $importo .  'divisa=' . $divisa . 'xpayNonce=' . $request['xpayNonce'] . 'timeStamp=' . $timeStamp . $this->secret);
+			$mac = sha1('apiKey=' . $this->apiKey . 'codiceTransazione=' . $codTrans  . 'importo=' . $importo .  'divisa=' . $divisa . 'xpayNonce=' . $xpayNonce . 'timeStamp=' . $timeStamp . $this->secret);
 			$client = new Client(['base_uri' => $this->url, RequestOptions::VERIFY => CaBundle::getSystemCaRootBundlePath()]);
 			$params = array(
 				'apiKey' => $this->apiKey,
 				'codiceTransazione' => $codTrans,
 				'importo' => $importo,
 				'divisa' => $divisa,
-				'xpayNonce' => $request['xpayNonce'],
+				'xpayNonce' => $xpayNonce,
 				'timeStamp' => (string) $timeStamp,
 				'mac' => $mac
 			);
@@ -52,7 +78,7 @@ class Secure3DPay extends Nexirequest
 			if ($myresponse['esito'] == 'OK') {
 				$macCalculated2 = sha1('esito=' . $myresponse['esito'] . 'idOperazione=' . $myresponse['idOperazione'] . 'timeStamp=' . $myresponse['timeStamp'] . $this->secret);
 				if ($macCalculated2 != $myresponse['mac']) {
-					throw new \Exception('Errore MAC:' . $macCalculated .' differs from'. $request['mac'],004);
+					throw new \Exception('Errore MAC:' . $macCalculated2 .' differs from'. $myresponse['mac'],004);
 				}
 				$result = [
 					'success' => 1,
