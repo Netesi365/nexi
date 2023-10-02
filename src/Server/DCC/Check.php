35af977ff7cf20ac809f686de10cb1065dfe50fe
unknown <cto@netesi365.com>
2023-10-02 17:12:11 +0200

    Added DCC\Check Method

---
+++ src/Server/DCC/Check.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Netesi365\Nexi\Server\DCC;
+
+use Netesi365\Nexi\Nexirequest;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+use GuzzleHttp\Exception\BadResponseException;
+use Composer\CaBundle\CaBundle;
+
+class Check extends Nexirequest
+{
+	public function action(string $importo = '', string $pan = '') : string {
+		try {
+			$timeStamp = (time()) * 1000;
+			$mac = sha1('apiKey=' . $this->apiKey . 'pan=' . $pan . 'importo=' . $importo . "timeStamp=" . $timeStamp . $this->secret);
+			$client = new Client(['base_uri' => $this->url, RequestOptions::VERIFY => CaBundle::getSystemCaRootBundlePath()]);
+			$params = array(
+				'apiKey' => $this->apiKey,
+				'pan' => $pan,
+				'importo' => $importo,
+				'timeStamp' => (string) $timeStamp,
+				'mac' => $mac
+			);
+			$guzrequest = $client->request('POST', '/ecomm/api/etc/verificaDCC', [
+				'connect_timeout' => 1.5,
+				'headers' => [
+					'Accept' => 'application/json',
+					'Content-Type' => 'application/json',
+				],
+				'body'=>json_encode($params,JSON_UNESCAPED_UNICODE)
+			]);
+			$guzresponse = $guzrequest->getBody()->getContents();
+			$myresponse = json_decode($guzresponse, true);
+			if (json_last_error() === JSON_ERROR_NONE) {
+				$MACrisposta = sha1('esito=' . $myresponse['esito'] . 'idOperazione=' . $myresponse['idOperazione'] . 'timeStamp=' . $myresponse['timeStamp'] . $this->secret);
+				if ($myresponse['mac'] == $MACrisposta) {
+					if ($myresponse['esito'] == 'OK') {
+						$result = [
+							'success' => 1,
+							'error' => 0,
+							'code' => '0',
+							'idOperazione' => $myresponse['idOperazione'],
+							'timeStamp' => $myresponse['timeStamp'],
+							'mac' => $myresponse['mac'],
+							'msg' => $myresponse['esito']
+						];
+					}
+					else {
+						throw new \Exception($myresponse['errore']['messaggio'], $myresponse['errore']['codice']);
+					}
+				}
+				else {
+					throw new \Exception('Failed Mac verification code', 002);
+				}
+			}
+			else {
+					throw new \Exception('Wrong response', 001);
+			}
+		}
+		catch (\Exception $e) {
+			$result = [
+					'success' => 0,
+					'error' => 1,
+					'code' => $e->getCode(),
+					'idOperazione' => (!empty($myresponse['idOperazione']) ? $myresponse['idOperazione'] : ''),
+					'timeStamp' => (!empty($myresponse['timeStamp']) ? $myresponse['timeStamp'] : ''),
+					'msg' => $e->getMessage()
+			];
+		}
+		catch (BadResponseException $e) {
+			$result = [
+					'success' => 0,
+					'error' => 1,
+					'code' => 0,
+					'idOperazione' => '',
+					'timeStamp' => (!empty($timeStamp) ? $timeStamp : ''),
+					'msg' => $e->getResponse()->getBody()->getContents()
+			];
+		}
+		$response = json_encode($result);
+		return $response;
+	}
+}
